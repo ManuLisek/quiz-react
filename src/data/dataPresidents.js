25dfcc55492db3cabe76b463947e1b9485e151ec
Michał Lisowiec <miclisowiec@gmail.com>
2021-03-12 20:05:07 +0100

    Add dataPresidents

---
+++ src/data/dataPresidents.js
@@ -0,0 +1,53 @@
+export const presidents = [
+  {
+    name: 'Gabriel',
+    lastName: 'Narutowicz',
+    termOfOffice: '1922'
+  },
+  {
+    name: 'Stanisław',
+    lastName: 'Wojciechowski',
+    termOfOffice: '1922-1926'
+  },
+  {
+    name: 'Ignacy',
+    lastName: 'Mościcki',
+    termOfOffice: '1926-1939'
+  },
+  {
+    name: 'Bolesław',
+    lastName: 'Bierut',
+    termOfOffice: '1947-1952'
+  },
+  {
+    name: 'Wojciech',
+    lastName: 'Jaruzelski',
+    termOfOffice: '1989-1990'
+  },
+  {
+
+    name: 'Lech',
+    lastName: 'Wałęsa',
+    termOfOffice: '1990-1995'
+  },
+  {
+    name: 'Aleksander',
+    lastName: 'Kwaśniewski',
+    termOfOffice: '1995-2005'
+  },
+  {
+    name: 'Lech',
+    lastName: 'Kaczyński',
+    termOfOffice: '2005-2010'
+  },
+  {
+    name: 'Bronisław',
+    lastName: 'Komorowski',
+    termOfOffice: '2010-2015'
+  },
+  {
+    name: 'Andrzej',
+    lastName: 'Duda',
+    termOfOffice: '2015- '
+  },
+]
