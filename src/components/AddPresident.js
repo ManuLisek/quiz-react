3bb8e3cada1a4dafafc10cc307098add9033abc2
Michał Lisowiec <miclisowiec@gmail.com>
2021-03-24 22:48:05 +0100

    Show guessed president

--- src/components/AddPresident.js
+++ src/components/AddPresident.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddPresident = () => {
-
+const AddPresident = (props) => {
 
   return (
     <div className="addPresident">
       <h1>Odgadnij wszystkich polskich prezydentów: </h1>
-      <input type="text" placeholder="Dodaj prezydenta..." />
+      <input type="text" placeholder="Dodaj prezydenta..." onChange={(e) => props.change(e)} />
     </div>
   );
 }
 
+AddPresident.propTypes = {
+    change: PropTypes.func,
+  };
 
 export default AddPresident;
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PresidentsList from './PresidentsList';
 import AddPresident from './AddPresident';
+import {presidents} from '../data/dataPresidents';
 import './App.scss';
 
 class App extends Component {
@@ -10,12 +11,26 @@ class App extends Component {
             isPresidentVisible: false,
         }
     }
+
+    handleChangeInput = (e) => {
+        const index = presidents.findIndex(president => (
+          president.lastName.toLowerCase() === e.target.value.toLowerCase() || `${president.name.toLowerCase()} ${president.lastName.toLowerCase()}` === e.target.value.toLowerCase()));
+        if(index !== -1){
+          presidents[index].isPresidentVisible = true;
+          const isPresidentVisible = presidents[index].isPresidentVisible;
+          this.setState(({result}) => ({
+            isPresidentVisible,
+            
+          }))
+          e.target.value = '';
+        }
+      }
     
   render(){
-    const {isPresidentVisible} = this.state
+    const {isPresidentVisible} = this.state;
     return (
       <div className="App">
-        <AddPresident />
+        <AddPresident change={this.handleChangeInput} />
         <PresidentsList isPresidentVisible={isPresidentVisible} />
       </div>
     );
